919d9411c2b03cd51720ede6b0bc4e6d3cdf5b2f
Paul <paul@originlab.com>
2026-04-24 16:04:47 +0800

    Add gen_utils.js

---
+++ static/gen_utils.js
@@ -0,0 +1,21 @@
+﻿function fetchApplyLayout(layoutUrl, containerId) {
+    window.addEventListener("DOMContentLoaded", async () => {
+        var layout = await fetch(layoutUrl);
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(await layout.text(), 'text/html');
+        var container = doc.getElementById(containerId);
+        container.innerHTML = document.body.innerHTML;
+        document.replaceChild(
+            document.adoptNode(doc.documentElement),
+            document.documentElement
+        );
+    })
+}
+
+function tryRedirectToLower() {
+    var currentURL = window.location.href;
+    var lowerCaseURL = currentURL.toLowerCase();
+    if (currentURL != lowerCaseURL) {
+        location.replace(lowerCaseURL);
+    }
+}
