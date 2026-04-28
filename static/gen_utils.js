b5678171618c463c6956a939ed03ed5762c07898
Paul Chen <lancevdance@gmail.com>
2026-04-28 11:20:30 +0800

    Fetch immediately

--- static/gen_utils.js
+++ static/gen_utils.js
@@ -1,15 +1,17 @@
-﻿function fetchApplyLayout(layoutUrl, containerId) {
-    window.addEventListener("DOMContentLoaded", async () => {
-        var layout = await fetch(layoutUrl);
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(await layout.text(), 'text/html');
-        var container = doc.getElementById(containerId);
-        container.innerHTML = document.body.innerHTML;
-        document.replaceChild(
-            document.adoptNode(doc.documentElement),
-            document.documentElement
-        );
-    })
+﻿function DOMContentLoadedEvent() {
+    return new Promise(resolve => window.addEventListener("DOMContentLoaded", event => resolve(event), { once: true }));
+}
+
+async function fetchApplyLayout(layoutUrl, containerId) {
+    var ps = await Promise.all([fetch(layoutUrl), DOMContentLoadedEvent()]);
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(await ps[0].text(), 'text/html');
+    var container = doc.getElementById(containerId);
+    container.innerHTML = document.body.innerHTML;
+    document.replaceChild(
+        document.adoptNode(doc.documentElement),
+        document.documentElement
+    );
 }
 
 function tryRedirectToLower() {
