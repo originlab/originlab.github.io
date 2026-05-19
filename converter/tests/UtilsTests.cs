2c70b99d855c31c7640a8a245ed137942b30c343
Paul Chen <lancevdance@gmail.com>
2026-05-19 12:15:51 +0800

    Extract converter.yml and add tests.

---
+++ converter/tests/UtilsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace OriginLab.DocumentGeneration.Tests;
+
+public class UtilsTests
+{
+    [Theory]
+    [InlineData("", new string[0])]
+    [InlineData("", new string?[] { null })]
+    [InlineData("", new[] { "" })]
+    [InlineData("", new[] { "", null })]
+    [InlineData("/a", new[] { "a", null })]
+    [InlineData("/a", new[] { "a", "" })]
+    [InlineData("/a/b", new[] { "a", "b" })]
+    [InlineData("/b", new[] { null, "b" })]
+    [InlineData("/b", new[] { "", "b" })]
+    public void TryPrefixEachSkipsEmptyItems(string expected, string?[] items)
+    {
+        Assert.Equal(expected, '/'.TryPrefixEach(items));
+    }
+
+    [Theory]
+    [InlineData("", new string[0])]
+    [InlineData("", new string?[] { null })]
+    [InlineData("", new[] { "" })]
+    [InlineData("", new[] { "", null })]
+    [InlineData("/a/", new[] { "a", null })]
+    [InlineData("/a/", new[] { "a", "" })]
+    [InlineData("/a/b/", new[] { "a", "b" })]
+    [InlineData("/b/", new[] { null, "b" })]
+    [InlineData("/b/", new[] { "", "b" })]
+    public void TrySurroundEachSkipsEmptyItems(string expected, string?[] items)
+    {
+        Assert.Equal(expected, '/'.TrySurroundEach(items));
+    }
+
+}
