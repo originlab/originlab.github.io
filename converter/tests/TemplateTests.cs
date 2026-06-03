5a4e493d8b914e58d2edaf752b0383960b017256
Paul Chen <lancevdance@gmail.com>
2026-06-03 09:57:25 +0800

    Add test for Template.

---
+++ converter/tests/TemplateTests.cs
@@ -0,0 +1,17 @@
+﻿using OriginLab.DocumentGeneration.Templates;
+
+namespace OriginLab.DocumentGeneration.Tests;
+
+public class TemplateTests
+{
+    [Theory]
+    [InlineData("de")]
+    [InlineData("ja")]
+    [InlineData("zh")]
+    public async Task NoEntitiesInThe404PageTitles(string language)
+    {
+        var html = await Template.Render404PageAsync(language);
+
+        Assert.Matches(@"<title>[^&#]*?</title>", html);
+    }
+}
