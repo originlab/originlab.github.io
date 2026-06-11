5efb1dae1aedabf33420a35be140f7f802c7916f
Paul Chen <lancevdance@gmail.com>
2026-06-11 14:19:33 +0800

    Reduce parameters.

--- converter/tests/ResourceResolverTests.cs
+++ converter/tests/ResourceResolverTests.cs
@@ -34,23 +34,24 @@ public class ResourceResolverTests
     }
 
     [Theory]
-    [InlineData("../images/A/a.jpg", "app", "en", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg", "app", "en", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg?v=123", "app", "en", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg?v=123", "app", "en", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg", "en", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg", "en", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg?v=123", "en", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg?v=123", "en", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
     // Image not exist in de but found in en
-    [InlineData("../images/A/a.jpg", "app", "de", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg", "app", "de", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg?v=123", "app", "de", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
-    [InlineData("../images/A/a.jpg?v=123", "app", "de", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg", "de", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg", "de", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg?v=123", "de", false, "/app/en/images/A/a.jpg?v=2a44c9a534ff6b5c")]
+    [InlineData("../images/A/a.jpg?v=123", "de", true, "/app/en/images/A/a.webp?v=2a44c9a534ff6b5c")]
     // Shared images
-    [InlineData("../images/A/Mini_bulb.png", "app", "en", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
-    [InlineData("../images/A/Mini_bulb.png", "app", "de", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
-    [InlineData("../images/A/Mini_bulb.png", "app", "de", true, "/books/images/Mini_bulb.webp?v=4b448e7cb64a0292")]
-    [InlineData("../images/A/Mini_bulb.png?v=123", "app", "de", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
-    [InlineData("../images/A/Mini_bulb.png?v=123", "app", "de", true, "/books/images/Mini_bulb.webp?v=4b448e7cb64a0292")]
-    public void SrcShouldResolveCorrectly(string src, string book, string language, bool useWebp, string expectedUrl)
+    [InlineData("../images/A/Mini_bulb.png", "en", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
+    [InlineData("../images/A/Mini_bulb.png", "de", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
+    [InlineData("../images/A/Mini_bulb.png", "de", true, "/books/images/Mini_bulb.webp?v=4b448e7cb64a0292")]
+    [InlineData("../images/A/Mini_bulb.png?v=123", "de", false, "/books/images/Mini_bulb.png?v=4b448e7cb64a0292")]
+    [InlineData("../images/A/Mini_bulb.png?v=123", "de", true, "/books/images/Mini_bulb.webp?v=4b448e7cb64a0292")]
+    public void SrcShouldResolveCorrectly(string src, string language, bool useWebp, string expectedUrl)
     {
+        var book = "app";
         var bookDir = Path.GetFullPath("../../../../converter/tests/books/" + book, AppContext.BaseDirectory);
         var args = new DocToStaticPagesTransformerArgs
         {
